12c38063370b35817ac2d9c5ffacf149a804efaa
Atharva1891 <106474240+Atharva1891@users.noreply.github.com>
2022-05-29 17:59:43 +0530

    Add files via upload

---
+++ Datatriend 1 live.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# First live lecture
+
+
+# In[5]:
+
+
+print("HELLO WORLD")
+
+
+# In[6]:
+
+
+n1 = input("numer 1")
+s = input("operaters(+,-,*,/)")
+n2 = input("number 2")
+n1 = int(n1)
+n2 = int(n2)
+if s== "+" :
+    print(n1+n2)
+elif s== "-" :
+    print(n1-n2)
+elif s== "*" :
+    print(n1*n2)
+elif s== "/" :
+    print(n1/n2)
+else:
+    print("not applied")
+
+
+# In[7]:
+
+
+type(n1)
+
+
+# In[11]:
+
+
+print(type(n1))
+
+
+# In[12]:
+
+
+hello_world = True
+hello_world
+
+
+# In[13]:
+
+
+t = (1,2,3,4,5)
+t
+
+
+# In[14]:
+
+
+print(type(t))
+
+
+# In[15]:
+
+
+t = list(t)
+t
+
+
+# In[ ]:
+
+
+
+
